76081bf1b76a9c0ba961b4530d82e174fb1ca1c2
pierangeloc <pierangeloc@gmail.com>
2013-08-27 23:15:17 +0200

    added a common service returning the data model. this service provides the data shared by both controllers.

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,18 @@
-function FirstCtrl($scope) {
+var myApp = angular.module('myApp', []); //myApp has no dependencies
+
+//myApp is a service, that can be injected in any model
+//'Data' is the name of the instance, the function is the getter function of the factory. The object Data  is the one returned
+// by the function
+myApp.factory('Data', function(){
+	return {message: "I'm being created by the service"};
+});
+
+
+function FirstCtrl($scope, Data) {
 //	$scope.data = {message:"alert-box"}; //data is a json object
+	$scope.data = Data;
 }
 
-function SecondCtrl($scope) {
-	
+function SecondCtrl($scope, Data) {
+	$scope.data = Data;
 }
