d546e1c63ff5b11cbc2a36ef87a4ce142515e019
pierangeloc <pierangeloc@gmail.com>
2013-08-30 00:14:16 +0200

    method in scope; alternative with a filter

--- app/js/main.js
+++ app/js/main.js
@@ -8,11 +8,24 @@ myApp.factory('Data', function(){
 });
 
 
+//create a filter, name it 'reverse'. In the first function you just inject any stuff that might be needed in the filter
+myApp.filter('reverse', function(Data) {
+	return function(text) {
+		return text.split("").reverse().join("***") + Data.message;
+	};
+});
+
 function FirstCtrl($scope, Data) {
 //	$scope.data = {message:"alert-box"}; //data is a json object
 	$scope.data = Data;
+	
+	$scope.reversedMessage = function(message) {
+		return message.split("").reverse().join("***");
+	};
 }
 
 function SecondCtrl($scope, Data) {
 	$scope.data = Data;
+	//expose a reversedMessage function
+	
 }
