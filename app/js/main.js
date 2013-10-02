a3ce671246eb57f2d184b311422b691d6d563c0e
pierangeloc <pierangeloc@gmail.com>
2013-10-03 00:03:04 +0200

    first experiments with directives. Just template and link

--- app/js/main.js
+++ app/js/main.js
@@ -5,8 +5,22 @@ var app = angular.module('superhero',[]);
 app.directive('superman', function(){
 	return {
 		restrict: 'A', //restrict to attribute, add feature to an existing element
+		//restrict: 'C', //restrict to class, add feature to an existing element
+
 		link: function() { //linking function, called when the attribute is encountered
-			alert('I am working!'); 
+			alert('I am working stronger');
 		}
-	}
-})
+//        template: "<div>Here I am to save your ass</div>"
+	};
+});
+
+app.directive('flash', function(){
+	return {
+		restrict: 'A', //restrict to attribute, add feature to an existing element
+		//restrict: 'C', //restrict to class, add feature to an existing element
+
+		link: function() { //linking function, called when the attribute is encountered
+			alert('I am working fast fast fast'); 
+		}
+	};
+});
