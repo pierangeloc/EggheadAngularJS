2edbbcb142021bbc41b6354f867b42c32403df5f
pierangeloc <pierangeloc@gmail.com>
2013-10-04 20:45:38 +0200

    using scope methods within a directive. Separating concerns between the scope and the directive

--- app/js/main.js
+++ app/js/main.js
@@ -6,12 +6,26 @@ app.controller("AppCtrl", function($scope){
     $scope.loadMoreTweets = function() {
         alert('Loading Tweets!');
     }
+
+    $scope.deleteTweets = function() {
+        alert('deleting tweets!');
+    }
 });
 
 app.directive('enter', function() {
     return function(scope, element, attrs) {
         element.bind('mouseenter', function () {
-           scope.loadMoreTweets()
+           //scope.$apply("loadMoreTweets()"); //$apply parses the string, and applies it on the scope. we can use also substitutions with {{}}
+            scope.$apply(attrs.enter);
+        });
+    }
+})
+
+app.directive('leave', function() {
+    return function(scope, element, attrs) {
+        element.bind('mouseleave', function () {
+            //scope.$apply("loadMoreTweets()"); //$apply parses the string, and applies it on the scope. we can use also substitutions with {{}}
+            scope.$apply(attrs.leave);
         });
     }
 })
